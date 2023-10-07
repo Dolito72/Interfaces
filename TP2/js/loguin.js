2419e7f8771ef85f499cff512f16d3f01e05d451
Maria Dolores Parral <doloresparral@hotmail.com>
2023-10-07 17:44:34 -0300

    agrego header para desktop, despliegues menues hamburguesa, usuario y carrito

--- TP2/js/loguin.js
+++ TP2/js/loguin.js
@@ -13,35 +13,35 @@
 
 //});
 // Obtener referencias a elementos HTML
-let mostrarConfetiBtn = document.getElementById("btn-loguin");
-let contenedorConfeti = document.getElementById("contenedorConfeti");
+//let mostrarConfetiBtn = document.getElementById("btn-loguin");
+//let contenedorConfeti = document.getElementById("contenedorConfeti");
 
 // Función para crear y mostrar confetis
 
 
-  function mostrarConfetis() {
-    for (let i = 0; i < 20; i++) { // creo 20 confetis
-        let confeti = document.createElement("div");
-        confeti.classList.add("confeti");
-        contenedorConfeti.appendChild(confeti);
+ // function mostrarConfetis() {
+ //   for (let i = 0; i < 20; i++) { // creo 20 confetis
+ //       let confeti = document.createElement("div");
+ //       confeti.classList.add("confeti");
+//        contenedorConfeti.appendChild(confeti);
 
         // Establece posiciones aleatorias para los confetis
-        const randomX = Math.random() * window.innerWidth;
-        const randomY = Math.random() * window.innerHeight;
-        confeti.style.left = randomX + "px";
-        confeti.style.top = randomY + "px";
-    }
-    contenedorConfeti.style.display = "flex"; // Mostrar el contenedor
-}
+ //       const randomX = Math.random() * window.innerWidth;
+ //       const randomY = Math.random() * window.innerHeight;
+ //       confeti.style.left = randomX + "px";
+ //       confeti.style.top = randomY + "px";
+//    }
+ //   contenedorConfeti.style.display = "flex"; // Mostrar el contenedor
+//}
 
 // Agregar un manejador de eventos al botón "Mostrar Confeti"
-mostrarConfetiBtn.addEventListener("click", function() {
-    mostrarConfetis();
+//mostrarConfetiBtn.addEventListener("click", function() {
+ //   mostrarConfetis();
 
     // Después de 5 segundos, redirigir a la página de "Loading"
-    setTimeout(function() {
-        window.location.href = "loading.html"; // me redirige a mi página de loading
-    }, 5000); // Duración de la animación de confeti (en milisegundos)
-});
+ //   setTimeout(function() {
+ //       window.location.href = "loading.html"; // me redirige a mi página de loading
+//    }, 3000); // Duración de la animación de confeti (en milisegundos)
+//});
 
 
