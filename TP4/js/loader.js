853de20a43a6c7da4e72692a8a36df4a0eb6eed3
Maria Dolores Parral <doloresparral@hotmail.com>
2023-11-28 17:14:47 -0300

    modifico algunas alineaciones

--- TP4/js/loader.js
+++ TP4/js/loader.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (progress >= 100) {
             clearInterval(interval);
             setTimeout(() => {
-                // Redirigir a la página de inicio después de 1 segundo (5000 ms en total)
+                // Redirigir a la página de inicio después de 1 segundo (1000 ms en total)
                 window.location.href = "home.html";
             }, 1000);
         }
